68a33170a19fea8aff216a083ab3208898548e31
Fursham Hamid <fursham.h@gmail.com>
2022-05-26 13:25:11 +0100

    New test for display functions

---
+++ tests/testthat/test_display.R
@@ -0,0 +1,18 @@
+data("factRsample")
+
+test_that("Test head/tail", {
+    expect_equal(factR2::head(factRsample), utils::head(factRsample@custom$ranges))
+    expect_equal(factR2::tail(factRsample), utils::tail(factRsample@custom$ranges))
+})
+
+test_that("Test View", {
+    expect_error(View(factRsample, "WRONGENE"))
+    expect_warning(View(factRsample, "WRONGENE", "Selenop"))
+})
+
+test_that("Test View", {
+    expect_error(Plot(factRsample, "WRONGENE"))
+    expect_warning(Plot(factRsample, "WRONGENE", "Selenop"))
+    plot <- Plot(factRsample, "Selenop")
+    expect_equal(c("patchwork", "gg","ggplot"), as.character(class(plot)))
+})
