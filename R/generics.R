cf59445cb12413316a3b6cc24d8be7da696388dd
Fursham Hamid <fursham.h@gmail.com>
2022-05-26 13:26:05 +0100

    Create new generics

--- R/generics.R
+++ R/generics.R
@@ -31,7 +31,7 @@ setClass("factR",
              reference = "list",
              ASplicings = "list",
              domains = "data.frame",
-             nmd = "list",
+             nmd = "data.frame",
              misc = "list",
              version = "character"
          )
@@ -41,63 +41,102 @@ setClass("factR",
 # Class generics
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#' Plot custom transcripts
+#' Preview custom transcriptome from factR object
+#'
+#' @description Test test
+#' @param object factRObject
+#' @param ... Optional: a list of features to view. Input
+#' can be a mixture of names from gene_id, gene_name or transcript_id metadata.
+#' If missing, function will return all features
+#' @param type data to show. Can be "transcripts" or "proteins"
+#' @param in_console whether to print custom transcriptome in console
+#'
+#' @export
+#' @rdname preview-methods
+#' @author Fursham Hamid
+setGeneric("View", function(object, ...,
+                            type = "transcripts",
+                            in_console = FALSE) standardGeneric("View"))
+
+#' @param object factRObject
+#' @param n an integer of the length to display
+#'
+#' @return GenomicRanges object
+#' @export
+#' @rdname preview-methods
+setGeneric("head", function(object, n = 6L) standardGeneric("head"))
+
+#' @param object factRObject
+#' @param n an integer of the length to display
+#'
+#' @export
+#' @rdname preview-methods
+setGeneric("tail", function(object,  n = 6L) standardGeneric("tail"))
+
+
+
+
+
+#' Visualize RNA transcripts or translated proteins
 #'
 #' @description
-#' A wrapper around wiggleplotr's plotTranscripts function.
-#' See the documentation for (\code{\link[wiggleplotr]{plotTranscripts}})
-#' for more information.
+#' Plots out transcript architecture or translated protein domains from
+#' selected genes
 #'
-#' @param object
-#' factRObject
-#'
-#' @param ...
-#' Character value of features to plot. Multiple features can be plotted by
-#' entering comma-delimited values. Features will be extracted from
-#' metadata gene_name, gene_id and transcript_id of the GTF. Can also be a
-#' conditional statement to filter values from variables in the GTF (e.g. gene_name == "Ptbp1")
-#'
-#' @param rescale_introns
-#' Specifies if the introns should be scaled to fixed length or not. (default: FALSE)
-#'
-#' @param ncol
-#' Number of columns to patch the output plots (default: 1)
+#' @param object  factRObject
+#' @param ... Optional: a list of features to plot. Input
+#' can be a mixture of names from gene_id, gene_name or transcript_id metadata.
+#' If missing, function will plot transcripts/proteins from the first 9 genes
+#' of the custom transcriptome
+#' @param type data to show. Can be "transcripts" or "proteins"
+#' @param rescale_introns when plotting transcripts, whether to rescale introns
+#' @param ncol number of columns to combine multiple feature plots to
 #'
 #' @return ggplot2 object. If multiple genes are detected, plots will be
 #' combined using patchwork
-#' @export Plot
+#' @export
 #' @rdname Plot
 #'
 #' @author Fursham Hamid
 #'
 #' @examples
-#' ## ---------------------------------------------------------------------
-#' ## EXAMPLE USING SAMPLE DATASET
-#' ## ---------------------------------------------------------------------
-#' # Load datasets
-#' data(query_gtf, ref_gtf)
+#' data(factRsample)
+#' View(factRsample, "Selenop")
+setGeneric("Plot", function(object, ..., type = "transcripts",
+                            rescale_introns = FALSE, ncol = 1) standardGeneric("Plot"))
+
+
+
+#' Test
 #'
-#' viewTranscripts(query_gtf)
-#' viewTranscripts(query_gtf, "transcript1")
-#' viewTranscripts(ref_gtf)
+#' @param object
+#' @param verbose
 #'
-#' ## ---------------------------------------------------------------------
-#' ## EXAMPLE USING TRANSCRIPT ANNOTATION
-#' ## ---------------------------------------------------------------------
-#' \donttest{
-#' library(AnnotationHub)
+#' @return
+#' @export
 #'
-#' ## Retrieve GRCm38 trancript annotation
-#' ah <- AnnotationHub()
-#' GRCm38_gtf <- ah[["AH60127"]]
-#'
-#' ## Plot transcripts from Ptbp1 gene
-#' viewTranscripts(GRCm38_gtf, "Ptbp1")
-#'
-#' # Plot transcripts from Ptbp1 and Ptbp2 genes
-#' viewTranscripts(GRCm38_gtf, "Ptbp1", "Ptbp2")
-#' }
-#'
-Plot <- function(object, ..., rescale_introns = FALSE, ncol = 1) {
-    UseMethod(generic = 'plot', object = object)
-}
+#' @examples
+setGeneric("RunfactR", function(object, verbose = FALSE) standardGeneric("RunfactR"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
