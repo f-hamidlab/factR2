abbbbd83834208c8bcc6859208a2d882f0e7075d
Fursham Hamid <fursham.h@gmail.com>
2022-06-07 10:13:07 +0100

    Simplified constructor function

--- R/constructor.R
+++ R/constructor.R
@@ -38,59 +38,28 @@ createfactRObject <- function(gtf, reference,
                               assays = "transcript",
                               verbose = FALSE){
 
-    # catch missing args
-    mandargs <- c("gtf")
-    passed <- names(as.list(match.call())[-1])
-    if (any(!mandargs %in% passed)) {
-        rlang::abort(paste(
-            "missing values for",
-            paste(setdiff(mandargs, passed), collapse = ", ")
-        ))
+    # check input GTF arguments
+    if(missing(gtf)){
+        rlang::abort("Missing gtf input")
     }
+
+    # check for reference arguments
+    annotation <- ""
+    genome <- ""
     if(missing(reference)){
         empty_custom_ref <- c(is.null(use_own_annotation), is.null(use_own_genome))
         if(all(empty_custom_ref)){
-            rlang::abort("missing value for reference")
+            rlang::abort("Missing value for reference")
         } else if(any(empty_custom_ref)){
             rlang::abort(sprintf("Using own reference, but value for '%s' not provided",
                 c("use_own_annotation", "use_own_genome")[empty_custom_ref]))
-        }
-    }
-
-    # check existence of GTF input and try to import
-    if(is.object(gtf)){
-        if(!is_gtf(gtf)){
-            rlang::abort("Input is not a GRanges GTF object")
         } else {
-            obj <- methods::new("factR")
-            obj@version <- factR2version
-            if(verbose){
-                rlang::inform("Reading custom GTF")
-            }
-            obj@transcriptome <- gtf  # import GTF
+            annotation <- use_own_annotation
+            genome <- use_own_genome
         }
     }
-    else if (!file.exists(gtf)){
-        rlang::abort(sprintf("File '%s' does not exist", gtf))
-    } else if (!grepl("gtf", tolower(gtf))){
-        rlang::abort(sprintf("File '%s' is not a GTF", gtf))
-    } else {
-        obj <- methods::new("factR")
-        obj@version <- factR2version
-        if(verbose){
-            rlang::inform("Importing custom GTF")
-        }
-        obj@transcriptome <- factR::importGTF(gtf)  # import GTF
-    }
-
-
-
-
-    # check for type of annotation and import
-    data(genomes)
-    # import supported reference
-    if(!missing(reference)){
-
+    else {
+        data(genomes)
         # select genome based on given reference
         if(reference %in% genomes$ID){
             selected_genome <- genomes[genomes$ID == reference,]
@@ -103,97 +72,45 @@ createfactRObject <- function(gtf, reference,
                                  reference))
         }
 
-        # get annotation
-        if(verbose){
-            rlang::inform("Retrieving supported reference")
-        }
-        custom.gtf <- .smartimport(selected_genome$annotation, ".gtf", verbose)
-        if(is_gtf(custom.gtf)){
-            obj@reference$ranges <- custom.gtf
-        } else {
-            rlang::abort("Annotation object is not a GTF")
-        }
-
-        # get genome
+        annotation <- selected_genome$annotation
         ## test if BSgenome object is installed
         if(selected_genome$genome.pri %in% BSgenome::installed.genomes()){
             require(selected_genome$genome.pri, character.only = T,
                     quietly = !verbose)
-            obj@reference$genome <- get(selected_genome$genome.pri)
+            genome <- get(selected_genome$genome.pri)
         } else {
-            obj@reference$genome <- .smartimport(selected_genome$genome.sec,
-                                                 ".fa",
-                                                 verbose)
+            genome <- selected_genome$genome.sec
         }
-
-        obj@reference$id <- selected_genome$ID
-        obj@reference$species <- selected_genome$species
-        obj@reference$db <- selected_genome$database
-    # import custom reference
-    } else {
-        # get custom annotation
-        if(verbose){
-            rlang::inform("Retrieving user-specified reference")
-        }
-
-        custom.gtf <- .smartimport(use_own_annotation, ".gtf", verbose)
-        if(is_gtf(custom.gtf)){
-            obj@reference$ranges <- custom.gtf
-        } else {
-            rlang::abort("Annotation object is not a GTF")
-        }
-
-        # get custom genome
-        obj@reference$genome <- .smartimport(use_own_genome, ".fa",
-                                             verbose)
-        obj@reference$id <- "custom"
-        obj@reference$species <- "custom"
-        obj@reference$db <- "custom"
     }
 
-    # match chromosomes and gene ID
-    if(verbose){
-        rlang::inform("Matching chromosome names")
-    }
-    obj@transcriptome <- factR::matchChromosomes(obj@transcriptome,
-                                                 obj@reference$genome)
-    obj@reference$ranges <- suppressWarnings(factR::matchChromosomes(obj@reference$ranges,
-                                                 obj@reference$ranges))
-    ## try find variables that contain gene ids
-    if(verbose){
-        rlang::inform("Matching gene names")
-    }
-    custom.df <- as.data.frame(obj@transcriptome)
-    potential_id_vars <- apply(custom.df, 2, function(x) any(grepl("ENS",x)))
-    potential_id <- names(potential_id_vars)[potential_id_vars]
-    potential_id <- potential_id[-which("transcript_id" %in% potential_id)]
-    if(length(potential_id > 1)){
-        if(verbose){
-            obj@transcriptome <- factR::matchGeneInfo(obj@transcriptome,
-                                                       obj@reference$ranges,
-                                                       primary_gene_id = "gene_id",
-                                                       secondary_gene_id = potential_id)
-        } else {
-            obj@transcriptome <- suppressMessages(
-                factR::matchGeneInfo(obj@transcriptome,
-                                     obj@reference$ranges,
-                                     primary_gene_id = "gene_id",
-                                     secondary_gene_id = potential_id))
+    # check existence of GTF input and try to import
+    if(is.object(gtf)){
+        if(!is_gtf(gtf)){
+            rlang::abort("Input is not a GRanges GTF object")
         }
-
+    }
+    else if (!file.exists(gtf)){
+        rlang::abort(sprintf("File '%s' does not exist", gtf))
+    } else if (!grepl(".gtf", tolower(gtf))){
+        rlang::abort(sprintf("File '%s' is not a GTF", gtf))
     } else {
         if(verbose){
-            obj@transcriptome <- factR::matchGeneInfo(obj@transcriptome,
-                                                      obj@reference$ranges,
-                                                      primary_gene_id = "gene_id")
-        } else {
-            obj@transcriptome <- suppressMessages(
-                factR::matchGeneInfo(obj@transcriptome,
-                                     obj@reference$ranges,
-                                     primary_gene_id = "gene_id"))
+            rlang::inform("Importing custom GTF")
         }
+        gtf <- factR::importGTF(gtf)
     }
+    # create new factRObject
+    obj <- methods::new("factR")
+    obj@version <- factR2version
+    obj@transcriptome <- gtf  # import GTF
+    custom.gtf <- .smartimport(annotation, ".gtf", verbose)
+    if(is_gtf(custom.gtf)){
+        obj@reference$ranges <- custom.gtf
+    } else {
+        rlang::abort("Annotation object is not a GTF")
     }
+    obj@reference$genome <- .smartimport(genome, ".fa",
+                                         verbose)
 
     return(obj)
 }
