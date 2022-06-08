0ca3d4c0f4e279df24881949f4f97f9215102c85
Fursham Hamid <fursham.h@gmail.com>
2022-06-08 16:40:03 +0100

    New test for prepTranscriptome

--- tests/testthat/test_constructor.R
+++ tests/testthat/test_constructor.R
@@ -19,16 +19,15 @@ test_that("Test correct object structure", {
     expect_equal(as.character(class(obj@reference$genome)), "BSgenome")
 })
 
-
+data('factRsample')
 test_that("Test .prepTranscriptome", {
-    expect_false(identical(obj@transcriptome, factRsample@transcriptome))
-    expect_equal(ncol(mcols(obj@transcriptome)), 12)
-    expect_equal(length(obj@sets), 3)
-    
-    expect_equal(nrow(obj@sets$transcript@rowData), 154)
-    expect_equal(nrow(obj@sets$gene@rowData), 41)
-    expect_equal(nrow(obj@sets$AS@rowData), 133)
-    
-    featdata <- featureData(obj[["transcript"]])
+    expect_equal(ncol(mcols(factRsample@transcriptome)), 12)
+    expect_equal(length(factRsample@sets), 3)
+
+    expect_equal(nrow(factRsample@sets$transcript@rowData), 154)
+    expect_equal(nrow(factRsample@sets$gene@rowData), 41)
+    expect_equal(nrow(factRsample@sets$AS@rowData), 133)
+
+    featdata <- featureData(factRsample[["transcript"]])
     expect_equal(nrow(featdata[featdata$novel == "yes",]), 46)
 })
