6c35ff79629332ab1be3c2471739f020e70b6ede
Fursham Hamid <fursham.h@gmail.com>
2022-06-10 16:32:21 +0100

    Improved constructors

--- R/constructor.R
+++ R/constructor.R
@@ -1,74 +1,100 @@
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Functions
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-#' Construct a factRObject
-#'
-#' @param gtf path to custom GTF file.
-#' @param reference a character value of supported reference ID. Run
-#' \code{\link{ListSupportedGenomes}} for a list of supported reference. Input
-#' can also be a reference species name ("Mouse" or "Mus musculus or "Mmusculus").
-#' To use your own annotation and genome, leave this parameter blank and provide
-#' inputs to `use_own_annotation` and `use_own_genome`
-#' @param use_own_annotation can be a (1) GenomicRanges object containing
-#' reference transcript annotation, (2) path to local GTF file, (3) AnnotationHub
-#' ID or (4) URL to an online GTF file.
-#' @param use_own_genome can be a (1) BSgenome or DNAStringSet object containing
-#' genome sequence, (2) path to local genome fasta file, (3) AnnotationHub
-#' ID or (4) URL to an online genome fasta file.
-#' @param verbose if TRUE, show progress messages
-#'
-#' @return factR object
-#' @export
-#'
-#' @examples
-#' # Create factRObject using sample GTF and supported reference
-#' gtf <- system.file("extdata", "sc_merged_sample.gtf.gz", package = "factR")
-#' obj <- createfactRObject(gtf, "vM25")
-#'
-#' # Create factRObject using custom reference
-#' library(BSgenome.Mmusculus.UCSC.mm10)
-#' obj <- createfactRObject(gtf, use_own_annotation = "AH49547", use_own_genome = Mmusculus)
 createfactRObject <- function(gtf, reference,
                               use_own_annotation = NULL,
                               use_own_genome = NULL,
-                              counts,
-                              psi,
-                              assays = "transcript",
+                              countData = NULL,
+                              sampleData = NULL,
+                              design = NULL,
+                              psi = NULL,
                               verbose = FALSE){
 
-    # check input GTF arguments
+    # check input arguments
+    if(verbose){
+        rlang::inform("Checking inputs")
+    }
+    ## GTF input
     if(missing(gtf)){
         rlang::abort("Missing gtf input")
     }
+    ## Annotation input
+    reference.list <- .checkannotation(reference, use_own_annotation,
+                                       use_own_genome, verbose)
 
-    # check for reference arguments
+    ## Count data input
+    if(!is.null(countData)){
+        # check if sampleData and design are given
+        if(any(c(is.null(sampleData), is.null(design)))){
+            args <- c("sampleData", "design")
+            args.null <- c(is.null(sampleData), is.null(design))
+            rlang::abort(sprintf("Counts data provided, but %s data missing",
+                                 paste(args[args.null], collapse = " and ")))
+        }
+    }
+
+
+    # create new factRObject
+    if(verbose){
+        rlang::inform("Creating factRObject")
+    }
+    obj <- methods::new("factR")
+    obj@version <- factR2version
+
+    # import data
+    if(verbose){
+        rlang::inform("Adding custom transcriptome")
+    }
+    obj@transcriptome <- .smartimport(gtf, ".gtf", "Custom transcriptome",verbose)
+
+    if(verbose){
+        rlang::inform("Adding annotation")
+    }
+    obj@reference$ranges <- .smartimport(reference.list[[1]], ".gtf", "Annotation",
+                                         verbose)
+    if(verbose){
+        rlang::inform("Adding genome sequence")
+    }
+    obj@reference$genome <- .smartimport(reference.list[[2]], ".fa", "Genome sequence",
+                                         verbose)
+
+
+    # run object prepration function
+    obj <- .prepfactR(obj, verbose)
+
+    # add and prep counts data if given
+    if(!is.null(countData)){
+        if(verbose){ rlang::inform("Adding expression counts data")}
+        obj <- addTxCounts(obj, countData, sampleData, design)
+    }
+
+    return(obj)
+}
+
+.checkannotation <- function(ref, own_annotation, own_genome, verbose){
     annotation <- ""
     genome <- ""
-    if(missing(reference)){
-        empty_custom_ref <- c(is.null(use_own_annotation), is.null(use_own_genome))
+    if(missing(ref)){
+        empty_custom_ref <- c(is.null(own_annotation), is.null(own_genome))
         if(all(empty_custom_ref)){
             rlang::abort("Missing value for reference")
         } else if(any(empty_custom_ref)){
             rlang::abort(sprintf("Using own reference, but value for '%s' not provided",
-                c("use_own_annotation", "use_own_genome")[empty_custom_ref]))
+                                 c("use_own_annotation", "use_own_genome")[empty_custom_ref]))
         } else {
-            annotation <- use_own_annotation
-            genome <- use_own_genome
+            annotation <- own_annotation
+            genome <- own_genome
         }
     }
     else {
         data(genomes)
         # select genome based on given reference
-        if(reference %in% genomes$ID){
-            selected_genome <- genomes[genomes$ID == reference,]
-        } else if(any(stringr::str_detect(genomes$synonyms, reference))){
+        if(ref %in% genomes$ID){
+            selected_genome <- genomes[genomes$ID == ref,]
+        } else if(any(stringr::str_detect(genomes$synonyms, ref))){
             selected_genome_index <- which( stringr::str_detect(genomes$synonyms,
-                                                                reference))
+                                                                ref))
             selected_genome <- genomes[selected_genome_index,]
         } else {
             rlang::abort(sprintf("Input reference '%s' not recognized",
-                                 reference))
+                                 ref))
         }
 
         annotation <- selected_genome$annotation
@@ -81,58 +107,38 @@ createfactRObject <- function(gtf, reference,
             genome <- selected_genome$genome.sec
         }
     }
+    return(list(annotation, genome))
 
-    # check existence of GTF input and try to import
-    if(is.object(gtf)){
-        if(!is_gtf(gtf)){
-            rlang::abort("Input is not a GRanges GTF object")
-        } else if(verbose){
-            rlang::inform("Using custom GRanges GTF object")
-        }
-    }
-    else if (!file.exists(gtf)){
-        rlang::abort(sprintf("File '%s' does not exist", gtf))
-    } else if (!grepl(".gtf", tolower(gtf))){
-        rlang::abort(sprintf("File '%s' is not a GTF", gtf))
-    } else {
-        if(verbose){
-            rlang::inform("Importing custom GTF")
-        }
-        gtf <- factR::importGTF(gtf)
-    }
-    # create new factRObject
-    obj <- methods::new("factR")
-    obj@version <- factR2version
-    obj@transcriptome <- gtf  # import GTF
-    
-    
-    custom.gtf <- .smartimport(annotation, ".gtf", verbose)
-    if(is_gtf(custom.gtf)){
-        obj@reference$ranges <- custom.gtf
-    } else {
-        rlang::abort("Annotation object is not a GTF")
-    }
-    
-    if(verbose){
-        rlang::inform("Retrieving reference genome")
-    }
-    obj@reference$genome <- .smartimport(genome, ".fa",
-                                         verbose)
-    
-    obj <- .prepTranscriptome(obj, verbose)
-
-    return(obj)
 }
 
-.smartimport <- function(input, format, verbose=FALSE){
+
+
+.smartimport <- function(input, format, type, verbose=FALSE){
     options(timeout = 10000)
 
     # import as an object
     if(is.object(input)){
+        if(format == ".gtf"){
+
+            #check if object is a GTF
+            if(!is_gtf(input)){
+                rlang::abort(sprintf("%s is not a GRanges GTF object", type))
+            }
+        }
+        if(verbose){
+            datatype <- as.character(class(input))
+            rlang::inform(sprintf("## Using %s object", datatype))
+        }
         return(input)
     }
+
+
+
     ## try local file
     else if (file.exists(input) & grepl(format, tolower(input))){
+        if(verbose){
+            rlang::inform("## Importing from local directory")
+        }
         if(format == ".gtf"){
             return(factR::importGTF(input))
         } else if(format == ".fa"){
@@ -143,6 +149,8 @@ createfactRObject <- function(gtf, reference,
     ## try AH
     else if(grepl("^AH", input)){
         if(verbose){
+            rlang::inform("## Importing from AnnotationHub")
+
             ah <- AnnotationHub::AnnotationHub()
             return(ah[[input]])
         } else {
@@ -153,6 +161,9 @@ createfactRObject <- function(gtf, reference,
     }
     ## try FTP
     else if(grepl("^http",input)){
+        if(verbose){
+            rlang::inform("## Importing from URL")
+        }
         if(format == ".gtf"){
             tmpfile <- tempfile()
             download.file(input, tmpfile, quiet = !verbose)
@@ -164,14 +175,13 @@ createfactRObject <- function(gtf, reference,
         }
 
     } else {
-        rlang::abort(sprintf("Input annotation '%s' not recognized",
-                             input))
+        rlang::abort(sprintf("Input not recognized or file does not exists"))
     }
 }
 
 
 
-.prepTranscriptome <-  function(object, verbose = FALSE) {
+.prepfactR <-  function(object, verbose = FALSE) {
     # match chromosomes and gene ID
     if(verbose){
         rlang::inform("Matching chromosome names")
@@ -201,7 +211,7 @@ createfactRObject <- function(gtf, reference,
                                      primary_gene_id = "gene_id",
                                      secondary_gene_id = potential_id))
         }
-        
+
     } else {
         if(verbose){
             object@transcriptome <- factR::matchGeneInfo(object@transcriptome,
@@ -214,7 +224,7 @@ createfactRObject <- function(gtf, reference,
                                      primary_gene_id = "gene_id"))
         }
     }
-    
+
     # Add 'gene' type in GTF
     if(!"gene" %in% object@transcriptome$type){
         genes.gtf <- object@transcriptome
@@ -241,46 +251,65 @@ createfactRObject <- function(gtf, reference,
             GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = T)
         object@transcriptome <- c(genes.gtf, object@transcriptome)
     }
-    
-    
+
+
     # create genetxs dataframe
     if(verbose){
-        rlang::inform("Creating sets")
+        rlang::inform("Creating factRset objects")
+    }
+    if(verbose){
+        rlang::inform("## Adding gene information")
     }
     object@active.set <- "transcript"
     object@sets$gene <- methods::new("factRset")
     object@sets$gene@rowData <- as.data.frame(object@transcriptome) %>%
         dplyr::filter(type %in% "gene") %>%
-        dplyr::select(gene_id, gene_name, width, match_level) %>%
+        dplyr::select(gene_id, gene_name, strand, width, match_level) %>%
         dplyr::distinct()
     rownames(object@sets$gene@rowData) <- object@sets$gene@rowData$gene_id
-    
+    object@sets$gene@counts <- as.matrix(data.frame(row.names =  rownames(object[["gene"]])))
+    object@sets$gene@data <- as.matrix(data.frame(row.names =  rownames(object[["gene"]])))
+
+    if(verbose){
+        rlang::inform("## Adding transcript information")
+    }
     object@sets$transcript <- methods::new("factRset")
     object@sets$transcript@rowData <- as.data.frame(object@transcriptome) %>%
         dplyr::filter(type %in% "exon") %>%
-        dplyr::group_by(gene_id, gene_name, transcript_id) %>%
+        dplyr::group_by(gene_id, gene_name, transcript_id, strand) %>%
         dplyr::summarise(width = sum(width)) %>%
+        dplyr::select(transcript_id, gene_id, gene_name, strand, width) %>%
         as.data.frame()
     rownames(object@sets$transcript@rowData) <- object@sets$transcript@rowData$transcript_id
-    
+    object@sets$transcript@counts <- as.matrix(data.frame(row.names =  rownames(object[["transcript"]])))
+    object@sets$transcript@data <- as.matrix(data.frame(row.names =  rownames(object[["transcript"]])))
+
+    if(verbose){
+        rlang::inform("## Adding Alternative splicing information")
+    }
     object@sets$AS <- methods::new("factRset")
-    object <- findAltSplicing(object)
+    object <- .findAS(object)
     object@sets$AS@rowData <- as.data.frame(object@transcriptome) %>%
         dplyr::filter(type %in% "AS") %>%
         dplyr::mutate(coord = paste0(seqnames, ":", start, "-", end)) %>%
-        dplyr::select(gene_id, gene_name, coord, AStype, width) %>%
+        dplyr::mutate(id = paste0(coord, ":", AStype, ":", strand, ":", gene_id)) %>%
+        dplyr::select(id, gene_id, gene_name, coord, AStype, strand, width) %>%
         dplyr::distinct()
-    rownames(object@sets$AS@rowData) <- paste0(object@sets$AS@rowData$coord,
-                                               ":",object@sets$AS@rowData$AStype)
-    
+    rownames(object@sets$AS@rowData) <- object@sets$AS@rowData$id
+    object@sets$AS@counts <- as.matrix(data.frame(row.names =  rownames(object[["AS"]])))
+    object@sets$AS@data <- as.matrix(data.frame(row.names =  rownames(object[["AS"]])))
+
     # annotate new transcripts
+    if(verbose){
+        rlang::inform("Annotating novel transcripts")
+    }
     newtxs <- suppressMessages(factR::subsetNewTranscripts(object@transcriptome,
                                                            object@reference$ranges,
                                                            refine.by = "intron"))
-    featureData(object)$novel <- ifelse(featureData(object)$transcript_id %in% newtxs$transcript_id,
-                                        "yes", "no")
-    featureData(object)$cds <- "no"
-    featureData(object)$nmd <- "no"
+    object@sets$transcript@rowData$novel <- ifelse(object[[]]$transcript_id %in% newtxs$transcript_id,
+                                                   "yes", "no")
+    object@sets$transcript@rowData$cds <- "no"
+    object@sets$transcript@rowData$nmd <- "no"
     object
 }
 
