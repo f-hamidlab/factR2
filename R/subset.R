47db13945ea5730bfeb155ba573e7b20632f61f7
Fursham <fursham.h@gmail.com>
2022-06-21 13:00:09 +0100

    Simplified subsetting columns

--- R/subset.R
+++ R/subset.R
@@ -2,20 +2,14 @@ select.factR <- function(object, ...){
     samples <- object@colData
     samples$old.names <- rownames(samples)
     samples <- as.data.frame(t(samples))
+    
+    samples <- tryCatch(
+        samples[,...],
+        error = function(e) 
+            dplyr::select(samples, ...))
 
     # perform col selection
-    samples <- as.data.frame(t(dplyr::select(samples, ...)))
+    object@colData <- as.data.frame(t(samples))
 
-    # correct counts data
-    for(i in listSets(object)){
-
-        if(!i %in% "AS"){
-            object@sets[[i]]@counts <- object@sets[[i]]@counts[,samples$old.names]
-            colnames(object@sets[[i]]@counts) <- rownames(samples)
-        }
-        object@sets[[i]]@data <- object@sets[[i]]@data[,samples$old.names]
-        colnames(object@sets[[i]]@data) <- rownames(samples)
-    }
-
-    return(object)
+    return(.updatefactR(object))
 }
