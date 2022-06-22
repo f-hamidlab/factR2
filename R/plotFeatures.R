8131385a093191ab2753d50729b23a6f43251d20
Fursham Hamid <fursham.h@gmail.com>
2022-06-22 16:21:14 +0100

    Added more plotting features

--- R/plotFeatures.R
+++ R/plotFeatures.R
@@ -1,7 +1,45 @@
-.testplot <- function(object, x, group.by = NULL, set = NULL){
+.testplot <- function(object, x, set = NULL, x.lab = NULL, x.lim = NULL,
+                      y.type = "count", y.lab = NULL,
+                      group.by = NULL, group.pos = "dodge"){
+    if(grepl("%", x)){
+        split <- str_split(x, "%")[[1]]
+        set <- split[1]
+        x <- split[2]
+    }
   dat <- features(object, set = set)
-  ggplot(dat, aes_string(x=x, fill = group.by)) + 
-    geom_bar() +
-    theme_minimal() + 
+  if(is.null(group.by)){
+      group.by = x
+  }
+
+
+  # base plot
+  plot <- ggplot(dat, aes_string(x=x, fill = group.by)) +
+    theme_minimal() +
     scale_fill_brewer(palette = "Set2")
+
+  # plot y-axis
+  if(y.type == "percent"){
+      plot <- plot +
+          geom_bar(position = "fill") +
+          scale_y_continuous(labels = scales::percent) +
+          ylab("percent")
+  } else{
+      plot <- plot +
+          geom_bar(position = group.pos)
+  }
+
+  # change axis labels
+  if(!is.null(x.lab)){
+      plot <- plot + xlab(x.lab)
+  }
+  if(!is.null(y.lab)){
+      plot <- plot + ylab(y.lab)
+  }
+
+  # change x-axis limits
+  if(!is.null(x.lim) & !class(dat[[x]]) %in% c("character","factor")){
+      plot <- plot + xlim(x.lim)
+  }
+
+      plot
 }
