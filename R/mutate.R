c28277e84469d06f773afdf96e9e14fae6c95440
Fursham Hamid <fursham.h@gmail.com>
2022-08-26 16:17:18 +0100

    Revamped mutate fun

--- R/mutate.R
+++ R/mutate.R
@@ -1,28 +1,49 @@
-mutate.factR <- function(object, ..., data = "samples"){
+#TODO: Check or refine this. Maybe change name
+mutate.factR <- function(object, meta = "samples", data=NULL, ...){
 
-    # check for type of data to modify
-    if(!data %in% c("gene", "transcript", "AS", "samples")){
-        rlang::abort("Incorrect input for `data`")
+    # check for type of meta to modify
+    if(!meta %in% c("gene", "transcript", "AS", "samples")){
+        rlang::abort("Incorrect input for `meta`")
     }
 
-    # get data
-    if(data == "samples"){
+    # get meta
+    if(meta == "samples"){
         df <- object@colData
     } else {
-        df <- object[[data]]
+        df <- object[[meta]]
     }
 
-    # store rownames and perform
-    names <- rownames(df)
-    df <- dplyr::mutate(df, ...)
-    rownames(df) <- names  # restore rownames
+    # Add dataframe to metadata if given
+    if(!is.null(data)){
+
+        # check for rownames
+        if(is.null(rownames(data))){
+            if(nrow(data) != nrow(df)){
+                rlang::abort("Length mismatch")
+            }
+        } else if(!all(rownames(data) %in% rownames(df))){
+            # create new data.frame with rownames from df
+            tokeep <- rownames(data)[rownames(data) %in% rownames(df)]
+            newdata <- data.frame(row.names = rownames(df))
+            newdata[tokeep,colnames(data)] <- data[tokeep,colnames(data)]
+            data <- newdata
+        } else{
+            data <- data[rownames(df),]
+        }
+
+        df <- dplyr::mutate(df, data)
+    } else{
+        # do dplyr mutate style transformation
+
+        df <- dplyr::mutate(df, ...)
+    }
 
 
-    # update data and return object
-    if(data == "samples"){
+    # update meta and return object
+    if(meta == "samples"){
         object@colData <- df
     } else {
-        object@sets[[data]]@rowData <- df
+        object@sets[[meta]]@rowData <- df
     }
     return(object)
 }
